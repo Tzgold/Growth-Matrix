5f0fb1109bcd43c021394accaa8b71556a5c6543
Tequam z <tequamzgold@gmail.com>
2026-01-17 22:13:03 +0300

    did some work

---
+++ d3/d3.js
@@ -0,0 +1,14 @@
+// ...existing code...
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7?module";
+
+async function loadData() {
+  const data = await d3.csv("amounts.csv");
+
+  data.forEach(d => {
+    d.amounts = +d.amounts;
+  });
+
+  console.log(data);
+}
+
+loadData();
