47033663bfe9b7ed3d82e41e9547eb19b2847153
Tequam z <tequamzgold@gmail.com>
2026-01-18 14:53:47 +0300

    another time

--- d3/d3.js
+++ d3/d3.js
@@ -12,4 +12,5 @@ async function loadData() {
 }
 //am balli 0n you whatch out wh=atch
 // for some reason
+//mey be in another life i will may be not watch this type of fucking stuff like to make my life better yk sometimes its out of my controll and the same as the rest yk
 loadData();
